d8ecb2aaed2912f95d00b85f42a8feee349949d8
jean-luc-jucker <jeanluc.jucker@gmail.com>
2020-05-23 17:32:25 +0200

    adding files (percent of population)

--- analysis.R
+++ analysis.R
@@ -46,6 +46,22 @@ data %>%
   theme(legend.title = element_blank())
 ggsave("figs/by_area_stacked.png") 
 
+#same, stacked proportional
+data %>% 
+  mutate(Stock=Stock/1000000) %>% 
+  filter(Area=="Africa" | Area=="Asia"| Area=="Europe"| Area=="Latin America and the Caribbean"| Area=="Northern America"| Area=="Oceania") %>% 
+  mutate(Area=recode(Area,'Latin America and the Caribbean'="Latin America")) %>% 
+  mutate(Area=reorder(Area, -Stock)) %>% 
+  ggplot(aes(x=Year, y=Stock, fill=Area))+ 
+  geom_area(position = "fill") +
+  scale_x_continuous(breaks = seq(0, 2020, by = 5))+
+  theme_bw()+
+  ylab("Migrant Stock (millions)")+
+  theme(axis.title.x = element_text(vjust=-1))+
+  theme(axis.title.y = element_text(vjust=2.5))+
+  theme(legend.title = element_blank())
+ggsave("figs/by_area_stacked.png") 
+
 #european countries (selection of 16)####
 data %>% 
   mutate(Stock=Stock/1000000) %>% 
@@ -76,6 +92,21 @@ data %>%
   theme(axis.title.x = element_text(vjust=-1))+
   theme(axis.title.y = element_text(vjust=2.5))+
   theme(legend.title = element_blank())
+
+#same, stacked proportional
+data %>% 
+  mutate(Stock=Stock/1000000) %>% 
+  filter(Area=="Finland" | Area=="Iceland"| Area=="Ireland"| Area=="Norway"| Area=="Sweden"| Area=="United Kingdom"| Area=="Greece"| Area=="Italy"| Area=="Portugal"| Area=="Spain"| Area=="Austria"| Area=="Belgium"| Area=="France"| Area=="Germany"| Area=="Netherlands"| Area=="Switzerland") %>% 
+  mutate(Area=reorder(Area, -Stock)) %>% 
+  ggplot(aes(x=Year, y=Stock, fill=Area))+ 
+  geom_area(position = "fill")+
+  scale_x_continuous(breaks = seq(0, 2020, by = 5))+
+  theme_bw()+
+  ylab("Migrant Stock (millions)")+
+  theme(axis.title.x = element_text(vjust=-1))+
+  theme(axis.title.y = element_text(vjust=2.5))+
+  theme(legend.title = element_blank())
+
 
 #africa
 africa <- data %>% 
@@ -169,7 +200,17 @@ world %>%
   ylab("Stock (millions)")+xlab("Country")+
   theme(axis.title.x = element_text(vjust=-1))
 
+#world, stacked proportiona
+
+world2 <- data %>% 
+  filter(as.numeric(as.character(Code))<=894) %>% 
+  mutate(Stock=Stock/1000000)
+
+world2 %>%
+  filter(Stock>7) %>% 
+  mutate(Area=reorder(Area, Stock)) %>% 
+  ggplot(aes(x=Year, y=Stock, fill=Area))+
+  geom_area(position = "fill")
 
 
-
-
+?geom_area
---
+++ unfpa_in_population_percent.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(readxl)
+library(plyr)
+getwd()
+
+data <- read_excel("data/UN_MigrantStockTotal_2019.xlsx", sheet = "Table 3", skip = 14)
+
+data <- data %>%
+  select(-c(1, 3, 5, 13:26)) %>%
+  rename(replace = c("Major area, region, country or area of destination" = "Area", "International migrant stock as a percentage of the total population (both sexes)" = "1990", "...7"="1995", "...8"="2000", "...9"="2005", "...10"="2010", "...11"="2015", "...12"="2019")) %>% 
+  filter(!is.na(Area)) %>% 
+  pivot_longer(cols = 3:9 , names_to = "Year", values_to = "Percent") %>% 
+  filter(Percent != "..")
+
+#convert to factor/numeric
+data[c(1:2)] <- lapply(data[c(1:2)], as.factor)
+data[c(3:4)] <- lapply(data[c(3:4)], as.numeric)
+
+save(data, file = "rda/unfpa_percent.rda") 
+
+#saving data in csv format too
+write.csv(data, "C:/Users/jeanl/projects/unfpa/rda/data_percent.csv")
+
+
